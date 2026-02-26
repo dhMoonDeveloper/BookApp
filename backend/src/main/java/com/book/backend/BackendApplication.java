107d7739cc0feaa1e5d25ce120d3182e8b761369
DONGHYUN_PC\xlr3z <dhmoon1023@gmail.com>
2026-02-26 18:03:05 +0900

    프로젝트 기본설정 변경

--- backend/src/main/java/com/book/backend/BackendApplication.java
+++ backend/src/main/java/com/book/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.realestate.backend;
+package com.book.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/com/book/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/book/backend/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.realestate.backend;
+package com.book.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
